37cfa5646b26806fcd5cc2e02587a9e462ecae7c
Guro Bokum <jiojiajiu@gmail.com>
2013-01-30 03:42:32 +0400

    шаблон settings

--- alljewel/settings.py
+++ alljewel/settings.py
@@ -5,30 +5,42 @@ TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
+    ('Guro', 'zzaupp@gmail.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'alljewel',                      # Or path to database file if using sqlite3.
+        'USER': 'alljewel',                      # Not used with sqlite3.
+        'PASSWORD': '12345',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    },
+
 }
 
+"""
+'vk': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'alljewel',
+    'USER': 'alljewel',
+    'PASSWORD': '12345',
+    'HOST': '',
+    'PORT': ''
+}
+"""
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Moscow'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-Ru'
 
 SITE_ID = 1
 
@@ -116,10 +128,20 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'product',
 )
+
+DATABASE_ROUTERS = [
+    'alljewel.modules.router.VKRouter',
+]
+
+UPLOAD_TO = {
+    'product': 'media/product',
+    'gem': 'media/gem',
+}
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
