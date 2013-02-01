5f4644b387d50e00add8d58051ab5f406e7c901e
Guro Bokum <jiojiajiu@gmail.com>
2013-02-01 04:38:45 +0400

    urls

--- alljewel/urls.py
+++ alljewel/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns, static
+
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -17,3 +20,7 @@ urlpatterns = patterns('',
     url(r'^products/', include('product.urls')),
     url(r'^shops/', include('shop.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('product.views', 
     url(r'^$', 'list'),
-    url(r'^id(?P<id>\d+)$', 'product'),
+    url(r'^id(?P<id>\d+)$', 'product', name='product_item'),
 )
