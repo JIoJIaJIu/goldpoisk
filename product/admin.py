667fde5a5c30cb71984d0ae94e933f5b5950852a
Guro Bokum <jiojiajiu@gmail.com>
2014-05-02 16:31:32 +0700

    Adding /static/, /media/

--- product/admin.py
+++ product/admin.py
@@ -1,14 +1,4 @@
 from django.contrib import admin
 from product.models import Product, Item, Type, Gem, Material, Image
 
-class ImageInline(admin.StackedInline):
-    model = Image
-
-class ProductAdmin(admin.ModelAdmin):
-    inlines = [ ImageInline ]
-
-admin.site.register(Product, ProductAdmin)
 admin.site.register(Item)
-admin.site.register(Type)
-admin.site.register(Gem)
-admin.site.register(Material)
