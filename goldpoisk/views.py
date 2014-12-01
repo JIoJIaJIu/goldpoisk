6d919053be260ce9e81a3924f1fa4d34fbe3045d
Guro Bokum <jiojiajiu@gmail.com>
2014-12-01 13:36:52 +0300

    Shuffle promo

--- goldpoisk/views.py
+++ goldpoisk/views.py
@@ -7,6 +7,8 @@ from PyV8 import JSClass, JSArray
 from pybem import pybem
 from  goldpoisk import templates
 
+from random import shuffle
+
 class JS(JSClass):
     def log(self, *args):
         print args
@@ -14,10 +16,17 @@ class JS(JSClass):
 renderer = pybem.BEMRender(os.path.abspath(settings.TEMPLATE_DIRS[0]), toplevelcls=JS)
 
 def main(req):
+    promo = [
+        'media/promotion/promotion_01.jpg',
+        'media/promotion/promotion_02.jpg',
+        'media/promotion/promotion_03.jpg',
+    ]
+    shuffle(promo)
+
+
     html = renderer.render('index', {
         'menu': templates.MENU,
-        'promo': ['','', ''],
-        'css': JSArray([])
+        'promo': promo
     }, req, 'blocks.page');
     res = HttpResponse(html);
     return res
