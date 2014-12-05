39242fca5937662b513ae34497dcc3e1357e4c1a
Guro Bokum <jiojiajiu@gmail.com>
2014-12-05 19:10:15 +0300

    deploy.sh delete ssh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-ssh webfaction "
-cd goldpoisk
+cd goldpoisk;
 
-mkdir -p arhives
-mv myproject arhives/myproject.${BUILD_NUMBER}
+mkdir -p arhives;
+mv myproject arhives/myproject.${BUILD_NUMBER};
 
-echo 'Stopping uwsgi..'
-uwsgi = $(ps -ef | grep [w]sgi | awk '{print $2}')
-
-echo 'processes' $uwsgi
-if [ -n $uwsgi ];then
-    kill $(ps -ef | grep [w]sgi | awk '{print $2}')
+echo 'Stopping u[w]sgi..';
+pr=$(ps -ef | grep [w]sgi | awk '{print $2}')
+if [ -n "$pr" ];then
+    echo 'processes' $pr
+    kill $pr
     sleep 1
 fi
 
@@ -24,9 +22,9 @@ echo 'Migration product..'
 cp -r arhives/myproject.${BUILD_NUMBER}/goldpoisk/product/migrations myproject/goldpoisk/product/
 python myproject/manage.py makemigrations product -v 2
 python myproject/manage.py migrate product -v 2
+sleep 1
 
 rm backend.zip
 echo 'Restart uwsgi && nginx..'
 uwsgi --master ~/golpoisk/myproject/production.ini
 ./nginx/sbin/nginx -s reload
-"
