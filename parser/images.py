e727d65e25bc7d2014a4a377c1cb7e3fbc36d82b
Guro Bokum <jiojiajiu@gmail.com>
2015-04-27 12:42:33 +0700

    Update parser images

--- parser/images.py
+++ parser/images.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 import MySQLdb as mdb
 from md5 import md5
 from _mysql_exceptions import OperationalError
@@ -15,9 +18,14 @@ def save_images (cursor, con):
     cursor.execute('SELECT * from goldpoisk_entity_images')
     keychain = []
     images = cursor.fetchall()
+    desc = [d[0].decode('utf-8') for d in cursor.description]
+    if os.path.exists('product'):
+        shutil.rmtree('product')
+    os.mkdir('product')
+
     for i, image in enumerate(images):
-        id = image[2]
-        blob = image[1]
+        id = image[desc.index('id')]
+        blob = image[desc.index('image')]
 
         hash = md5(blob).hexdigest()
         keychain.append(hash)
@@ -25,7 +33,7 @@ def save_images (cursor, con):
         src = 'product/%s.jpg' % hash
         print '> %d > %s' % (id, src)
         f = open(src, 'w+')
-        f.write(image[1])
+        f.write(blob)
         sql = 'UPDATE goldpoisk_entity_images set src="%s" where id=%d;' % (src, id)
         cursor.execute(sql)
         f.close()
@@ -34,11 +42,16 @@ def save_images (cursor, con):
 
 def alter_table (cursor):
     try:
+        cursor.execute('SELECT id from goldpoisk_entity_images')
+    except OperationalError as e:
+        cursor.execute('ALTER TABLE goldpoisk_entity_images ADD id INT AUTO_INCREMENT UNIQUE');
+        print 'Successuful adding `id` to table'
+
+    try:
         cursor.execute('SELECT src from goldpoisk_entity_images')
-        print 'Not needed alter table'
     except OperationalError as e:
         cursor.execute('ALTER TABLE goldpoisk_entity_images ADD src CHAR(128)');
-        print 'Successuful alter table'
+        print 'Successuful adding `src` to table'
 
 if __name__ == '__main__':
     con = connect()
