6d324e202c6138fb62f90972849a2f3e7c96fb87
Guro Bokum <jiojiajiu@gmail.com>
2015-04-27 12:42:58 +0700

    Update parser config

--- parser/updater/config.py
+++ parser/updater/config.py
@@ -2,19 +2,22 @@ import time
 
 TYPES = {
     'rings': 1,
+    'bracelets': 2,
     'necklace': 3,
+    'chains':4,
     'pendants': 5,
-    'brooches': 7,
     'earrings': 6,
-    'watches': 8
+    'brooches': 7,
+    'watches': 8,
 }
 
 SHOPS = {
+    'gold585': 2,
     'Sunlight': 6,
 }
 
-TYPE_KEY = 'earrings'
-TYPE_SHOP = 'Sunlight'
+TYPE_KEY = 'bracelets'
+TYPE_SHOP = 'gold585'
 
 TYPE_ID = TYPES[TYPE_KEY]
 SHOP_ID = SHOPS[TYPE_SHOP]
